fe841ac7d7212fd26ce0f8cb310d6952a45095cf
Bart Kessels <bartkessels@bk-mail.com>
2017-07-10 20:31:04 +0200

    Fix wrong e-mail address in license

--- src/ag-application.c
+++ src/ag-application.c
@@ -1,7 +1,7 @@
 /*
  * ag-application.c
  *
- * Copyright (C) 2017 Bart Kessels <bartkessels@outlook.com>
+ * Copyright (C) 2017 Bart Kessels <bartkessels@bk-mail.com.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
